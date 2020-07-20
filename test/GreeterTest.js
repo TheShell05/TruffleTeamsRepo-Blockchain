1300858200e51f8ae85f5d270b15f2d28b25b610
Harsh Oza <42001739+HarshOza36@users.noreply.github.com>
2020-07-20 16:06:52 +0530

    Update GreeterTest.js

--- test/GreeterTest.js
+++ test/GreeterTest.js
@@ -6,17 +6,18 @@ contract(
       const greeter = await GreeterContract.deployed();
       assert(greeter, "contract wasn't deployed");
     });
-  },
-  describe("greet()", () => {
-//     it("returns Good Evening", async () => {
-      it("Good Evening", async () => {
-      const greeter = await GreeterContract.deployed();
-      const expected = "Good Evening";
-      const actual = await greeter.greet();
-      assert.equal(actual, expected, "Greeting Should be 'Good Evening'");
-    });
-  })
-);
+  });
+//   },
+//   describe("greet()", () => {
+// //     it("returns Good Evening", async () => {
+//       it("Good Evening", async () => {
+//       const greeter = await GreeterContract.deployed();
+//       const expected = "Good Evening";
+//       const actual = await greeter.greet();
+//       assert.equal(actual, expected, "Greeting Should be 'Good Evening'");
+//     });
+//   })
+// );
 
 // function testUserGreeter() {
 //   let returnGreet = GreeterContract.greet("Good evening");
